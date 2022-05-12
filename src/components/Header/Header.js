8e5ce827e8abc08f566305e563a7a4d882baa766
dhrubobari <bari.dhrubo@gmail.com>
2022-05-12 21:22:48 +0600

    banner added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import './Header.css';
 
 const Header = () => {
-    return (
-           <div>
-               <h2>header</h2>
-           </div>
-    );
+  return (
+    <>
+      <Navbar bg="success" variant="dark">
+      <Container>
+          <Navbar.Brand to="/">
+            Tech Scope
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/home">Home</Nav.Link>
+              <Nav.Link href="/items">Items</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
 };
 
 export default Header;
---
+++ src/components/Home/Banner/Banner.js
@@ -0,0 +1,25 @@
+import { Carousel } from "react-bootstrap";
+import React from "react";
+import tv from '../../../tv.jpeg';
+
+const Banner = () => {
+  return (
+    <>
+    <Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={tv}
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>Find The Best Tv For You</h3>
+      <p>Spend more time with your family.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+    </>
+  );
+};
+
+export default Banner;
