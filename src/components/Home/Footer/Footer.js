49d7e41f6323859708bba103aa8a887ef0627eca
dhrubobari <bari.dhrubo@gmail.com>
2022-05-13 01:08:46 +0600

    footer added

---
+++ src/components/Home/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className='copyright'>
+            <p className='mt-5'>Tech Scope © 2022</p>
+        </div>
+    );
+};
+
+export default Footer;
